c454a5f1d784784515ee564d8bcf2e15326fb71c
epaolell <epaolell@stevens.edu>
2025-11-07 11:38:16 -0500

    Week 2: Viewer Prototype
    In viewer.py, I added some simple code to the function load_model in order to make it show a 3D model. It requires an absolute path at the moment.
    IMPORTANT: One line, self.add_axes_widget() was not letting me test loading the model, so I commented it out for now.
    The load_model function is called in the __init__ function.
    
    In main_window.py, I added code that can hopefully help us with week 3. It is all in docstrings, so it should not effect anything.

--- app/viewer.py
+++ app/viewer.py
@@ -2,32 +2,62 @@
 3D viewer using PyVista
 widget for displaying 3D models in the main window
 """
-from PyQt6.QtWidgets import QWidget
+import sys
+
+from PyQt6.QtWidgets import QWidget, QApplication
 from PyQt6.QtCore import Qt
 import pyvista as pv
+from pyvista import examples
 from pyvistaqt import QtInteractor
 
 
 class ThreeDViewer(QtInteractor):
     """3D model viewer widget"""
-    
+
     def __init__(self, parent=None):
         """init viewer"""
         super().__init__(parent)
-        self.setup_renderer()
-    
+        self.setup_renderer() # calls the function to set up renderer settings
+
+        self.plotter = pv.Plotter() # sets up a variable to hold the 3D model plot
+
+        # this calls the load_model function, with the file path as an input
+        self.load_model('C:\\Users\lizzy\OneDrive\Documents\GitHub\\3D-visual\\assets\models\cone.obj')
+        # need the ABSOLUTE location for this to work ^^ (this would be mine)
+
+        self.clear() # clears everything
+
     def setup_renderer(self):
         """setup renderer settings"""
         self.background_color = '#2b2b2b'
         self.enable_trackball_style()
-        self.add_axes_widget()
-    
+        #self.add_axes_widget() # ERROR for some reason, this would not let it run
+
     def load_model(self, file_path: str) -> bool:
         """load and show a 3D model"""
-        # TODO: implement in week 2 - load from assets folder
-        pass
-    
+        """
+        This function takes in a file path, reads it with Pyvista, and them displays it in a new window
+        param: file_path -- the absolute path of the file that contains the 3D model
+        """
+
+        print("reading file path", file_path)
+        mesh = pv.read(file_path)
+        self.plotter.add_mesh(mesh)
+        self.plotter.show()
+
+        return
+
     def clear(self):
         """clear all models"""
         self.renderer.clear()
+        #self.plotter.clear()
 
+"""
+# to test if it works -- remove quotations
+app = QApplication(sys.argv) # main app setup?
+
+window = ThreeDViewer() # variable to hold main window
+window.show() # IMPORTANT -- so we can actually see it
+
+app.exec_() # executing the app
+"""
